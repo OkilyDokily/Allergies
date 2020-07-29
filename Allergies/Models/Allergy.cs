2a769201db583b8f02be33660f32a7dc717097cc
me <me@me.me.com>
2020-07-29 13:58:23 -0700

    create a dictionary

---
+++ Allergies/Models/Allergy.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Allergies{
+    public class Allergy{
+      private Dictionary<int, string> ad = new Dictionary<int,string>(){
+        {1,"eggs"},{2,"peanuts"},{4,"shellfish"},{8,"strawberries"},{16,"tomatoes"},{32,"chocolate"},{64,"pollen"},{128,"cats"}
+      };
+
+      public int getHighestDouble(int){
+        return 0;
+      } 
+    }
+}
